d0753845ca7e4b5421e5e4dd17a90c74591eee42
LunaStev <youngjae681@gmail.com>
2025-01-30 16:37:18 +0900

    x86_64 Codegen Initial Code

--- src/codegen/x86_64.rs
+++ src/codegen/x86_64.rs
@@ -1,1 +1,11 @@
 pub struct X86_64Codegen;
+
+impl X86_64Codegen {
+    pub fn new() -> Self {
+        X86_64Codegen {}
+    }
+
+    pub fn generate(&self) {
+        println!("Generating x86_64 code");
+    }
+}
