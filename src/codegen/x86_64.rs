3cddfe4f8b2c4457367f157b51301fe806fd81b0
LunaStev <youngjae681@gmail.com>
2025-03-29 10:51:17 +0900

    Default ELF Text Section

--- src/codegen/x86_64.rs
+++ src/codegen/x86_64.rs
@@ -3,12 +3,23 @@ use crate::wir::{instruction::Instruction, WIRModule};
 pub fn generate(module: &WIRModule) -> String {
     let mut asm = String::new();
 
-impl X86_64Codegen {
-    pub fn new() -> Self {
-        X86_64Codegen {}
-    }
+    // Default ELF Text Section
+    asm.push_str("section .text\n");
+    asm.push_str("global _start\n\n");
+    asm.push_str("_start:\n");
 
-    pub fn generate(&self) {
-        println!("Generating x86_64 code...");
+    for instr in module.get() {
+        match instr {
+            Instruction::Print { format, args } => {
+                asm.push_str(&generate_print(format, args));
+            }
+            Instruction::Store { name, value } => {
+                // For testing: Store first annotate
+                asm.push_str(&format!("  ; Store {} = {:?}\n", name, value));
+            }
+            _ => {
+                asm.push_str("  ; [UNIMPLEMENTED INSTRUCTION]\n");
+            }
+        }
     }
 }
