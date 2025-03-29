f7778e012e688c30b0a25d48bfcc685d0fbc620a
LunaStev <youngjae681@gmail.com>
2025-03-29 10:48:37 +0900

    use dummy, instruction

--- src/wir/builder.rs
+++ src/wir/builder.rs
@@ -1,4 +1,9 @@
-pub struct WIRBuilder;
+use crate::dummy;
+use crate::dummy::{AST, Statement};
+use super::{instruction::Instruction, value::Value, WIRModule};
+
+pub fn build(ast: AST) -> WIRModule {
+    let mut module = WIRModule::new();
 
 impl WIRBuilder {
     pub fn new() -> Self {
