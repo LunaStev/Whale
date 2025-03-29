19686ed92ee5a819a77ad307e49ddcb25674dfae
LunaStev <youngjae681@gmail.com>
2025-03-29 10:49:43 +0900

    compilation of nasm

--- src/elf/writer.rs
+++ src/elf/writer.rs
@@ -1,4 +1,11 @@
-pub struct ELFWriter;
+use std::fs::File;
+use std::io::Write;
+use std::process::Command;
+
+pub fn write_elf(asm: &str, output_name: &str) {
+    // Step 1: Save .asm files
+    let mut asm_file = File::create("temp.asm").expect("Failed to create temp.asm");
+    asm_file.write_all(asm.as_bytes()).expect("Failed to write asm");
 
 impl ELFWriter {
     pub fn new() -> Self {
